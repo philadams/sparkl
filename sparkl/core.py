352c7ae2a3d7a63dd45ec1d7c814b1a04c87ecb6
Phil Adams <phil@philadams.net>
2013-01-14 19:03:45 -0500

    handle stdin or arg list

--- sparkl/core.py
+++ sparkl/core.py
@@ -8,6 +8,8 @@ sparkl. http://github.com/philadams/sparkl
 """
 
 import logging
+import os
+import fileinput
 
 ticks = u'▁▂▃▅▆▇'
 
@@ -20,8 +22,10 @@ def sparkl():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--verbose', '-v', action='count', default=0)
     parser.add_argument('--delimiter', '-d', default=' ')
-    #parser.add_argument('--zero-min', '-z', default='' action='store_false')
-    parser.add_argument('data', nargs=argparse.REMAINDER)
+    #parser.add_argument('--zero-min', '-z', default=''
+                         #action='store_false')
+    parser.add_argument('data', nargs=argparse.REMAINDER,
+                        default='-')
     args = parser.parse_args()
 
     # logging config
@@ -35,14 +39,23 @@ def sparkl():
     # main debuggery
     logging.debug('args: %s' % args.__repr__())
 
-    # not yet implemented
-    if args.delimiter is not ' ':
-        raise NotImplementedError, '--delimiter/-d flag coming soon...'
+    data = []
+    if os.isatty(0) and not args.data:
+        parser.print_help()
+        exit()
+    if args.data:
+        data = args.data
+    else:
+        data = [line.strip() for line in fileinput.input()]
+
+    if len(data[0].split(args.delimiter)) > 1:
+        data = [int(x.strip()) for x in data[0].split(args.delimiter)]
+    else:
+        data = [int(x.strip()) for x in data if x.strip() is not '']
 
     # TODO: zero-min or data-min?
 
     # bin data into ticks
-    data = [int(i) for i in args.data]
     range_min = min(data)
     range_span = max(data) - range_min
     step = (range_span / float(len(ticks) - 1)) or 1
